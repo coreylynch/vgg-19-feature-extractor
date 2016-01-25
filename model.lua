539bde8857298f9b2c50cfc8f6b774ef03168284
Arjun Rajanna <arajanna@gpucomputron02.ny2.etsy.com>
2016-01-25 16:24:28 +0000

    adding parallelization

--- model.lua
+++ model.lua
@@ -1,6 +1,8 @@
 require 'nn'
 require 'optim'
 require 'loadcaffe'
+require 'ccn2'
+require 'util'
 
 if not paths.dirp('model_weights') then
 	print('=> Downloading VGG 19 model weights')
@@ -10,44 +12,81 @@ if not paths.dirp('model_weights') then
   os.execute('wget --output-document model_weights/VGG_ILSVRC_19_layers.caffemodel ' .. caffemodel_url)
   os.execute('wget --output-document model_weights/VGG_ILSVRC_19_layers_deploy.prototxt ' .. proto_url)
 end
-
+--opt ={}
+--opt.backend = "cunn"
+--opt.nGPU = 1
+--nGPU = opt.nGPU 
+--opt.GPU = 1
 --[[
    1. Load pre-trained model
    2. Remove last layer
    3. Convert model to CUDA
 ]]--
-function getPretrainedModel()
+function getPretrainedModel(nGPU)
   local proto = 'model_weights/VGG_ILSVRC_19_layers_deploy.prototxt'
   local caffemodel = 'model_weights/VGG_ILSVRC_19_layers.caffemodel'
 
   if opt.backend == 'cudnn' then
       require 'cudnn'
-  elseif backend == 'cunn' then
+  elseif opt.backend == 'cunn' then
+      require 'cunn'
       print('using cunn backend')
   else
       error('unrecognized backend: ' .. backend)
   end
 
-  local model = loadcaffe.load(proto, caffemodel, opt.backend)
+  local inputModel = loadcaffe.load(proto, caffemodel, 'ccn2')
 
 	--[[
 		Remove the Softmax, class scores, and dropout layer from the original
 		network, leaving only the ReLU-ed activations immediately prior to the
 		classifier.
 	]]--
+
 	for i=1,3 do
-		model.modules[#model.modules] = nil 
+		inputModel.modules[#inputModel.modules] = nil 
 	end
 
-	-- L2 normalize the activations
-  model:add(nn.Normalize(2))
+  --features = nn.Sequential() ; j=1; for i=40,45 do features:add(model.modules[i]) ; end
+  --j=45 ; for i=1,6 do model:remove(j) ; j=j-1; end
+
+  --final model
+  model = nn.Sequential() ;
+  
+  features = nn.Sequential() ;
+  classifier = nn.Sequential() ;
+  
+  features,classifier = modelParallel(inputModel)
+ 
+  features:cuda()
+  features = makeDataParallel(features,nGPU)
+  model:add(features):add(classifier)  
+  
+  --architecture 2 addition
+  model:add(nn.Linear(4096,1000))
+  model:add(nn.ReLU(true))
+  
+  --3rd archtiecture
+  model:add (nn.Linear(1000,20))
+
+  -- L2 normalize the activations
+  model:add(nn.Normalize(2))  
 
   model:evaluate()
 
   return model
 end
 
-model = getPretrainedModel()
+--make the model parallelizable
+--i.e features (to be parallel) and classifier (not parallel) 
+function modelParallel(net)
+  local classifier = nn.Sequential() ; 
+  for i=40,45 do classifier:add(net.modules[i]) ; end
+  j=45 ; for i=1,6 do net:remove(j) ; j=j-1; end
+ return net,classifier
+end
+
+model = getPretrainedModel(nGPU)
 print('=> Model')
 print(model)
 
